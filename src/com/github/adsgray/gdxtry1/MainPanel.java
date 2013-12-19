f64b47b9f923617edf24ba4bcb8a5baa2a5b7e89
Andrew Gray <adsgray@gmail.com>
2013-12-19 14:08:31 -0800

    stub gdx main game

--- src/com/github/adsgray/gdxtry1/MainPanel.java
+++ src/com/github/adsgray/gdxtry1/MainPanel.java
@@ -1,36 +1,23 @@
 package com.github.adsgray.gdxtry1;
 
+/*
 import android.content.Context;
 import android.graphics.Canvas;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+*/
 
 
-public class MainPanel extends SurfaceView implements SurfaceHolder.Callback {
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
-    public MainPanel(Context context) {
-        super(context);
-        // TODO Auto-generated constructor stub
-        setFocusable(true);
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder arg0) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder arg0) {
-        // TODO Auto-generated method stub
-        
-    }
+public class MainPanel implements ApplicationListener {
 
 }
