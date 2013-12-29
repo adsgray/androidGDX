3be2b10e30ddee8984f15d50171a51086231d85c
Andrew Gray <adsgray@gmail.com>
2013-12-29 12:27:02 -0800

    blob tests

---
+++ src/com/github/adsgray/gdxtry1/test/TestBlob.java
@@ -0,0 +1,71 @@
+package com.github.adsgray.gdxtry1.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.github.adsgray.gdxtry1.engine.accel.AccelIF;
+import com.github.adsgray.gdxtry1.engine.accel.LinearAccel;
+import com.github.adsgray.gdxtry1.engine.blob.BaseBlob;
+import com.github.adsgray.gdxtry1.engine.blob.BlobIF;
+import com.github.adsgray.gdxtry1.engine.position.PositionIF;
+import com.github.adsgray.gdxtry1.engine.velocity.VelocityIF;
+import com.github.adsgray.gdxtry1.game.AccelFactory;
+import com.github.adsgray.gdxtry1.output.Renderer;
+
+public class TestBlob {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testInit() {
+        Renderer r = TestFactory.renderer();
+
+        PositionIF posin = TestFactory.position42();
+        VelocityIF velin = TestFactory.velocity1dash1();
+        AccelIF accel = AccelFactory.zeroAccel();
+
+        BlobIF b = new BaseBlob(0, posin, velin, accel, r);
+        
+        assertEquals("init pos", posin, b.getPosition());
+        assertEquals("init vel", velin, b.getVelocity());
+        assertEquals("init accel", accel, b.getAccel());
+    }
+    
+    @Test
+    public void testVel() {
+        Renderer r = TestFactory.renderer();
+
+        PositionIF posin = TestFactory.position42();
+        VelocityIF velin = TestFactory.velocity1dash1();
+        AccelIF accel = AccelFactory.zeroAccel();
+
+        BlobIF b = new BaseBlob(0, posin, velin, accel, r);
+        b.tick();
+        
+        PositionIF blobPos = b.getPosition();
+        assertEquals("X pos after tick", 43, blobPos.getX());
+        assertEquals("Y pos after tick", 43, blobPos.getY());
+    }
+    
+    @Test
+    public void testAcc() {
+        Renderer r = TestFactory.renderer();
+
+        PositionIF posin = TestFactory.position42();
+        VelocityIF velin = TestFactory.velocity1dash1();
+        AccelIF accel = new LinearAccel(1,1);
+
+        BlobIF b = new BaseBlob(0, posin, velin, accel, r);
+        b.tick();
+        
+        VelocityIF blobVel = b.getVelocity();
+        assertEquals("X pos after tick", 2, (int)blobVel.getXVelocity());
+        assertEquals("Y pos after tick", 2, (int)blobVel.getYVelocity());
+    }
+
+}
