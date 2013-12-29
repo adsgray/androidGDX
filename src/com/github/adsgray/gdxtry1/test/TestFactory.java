42064bc25854807f88ad5882b6f83f1343debb3c
Andrew Gray <adsgray@gmail.com>
2013-12-28 16:27:22 -0800

    more test stuff

--- src/com/github/adsgray/gdxtry1/test/TestFactory.java
+++ src/com/github/adsgray/gdxtry1/test/TestFactory.java
@@ -2,6 +2,10 @@ package com.github.adsgray.gdxtry1.test;
 
 import com.github.adsgray.gdxtry1.engine.accel.AccelIF;
 import com.github.adsgray.gdxtry1.engine.accel.LinearAccel;
+import com.github.adsgray.gdxtry1.engine.blob.BlobIF;
+import com.github.adsgray.gdxtry1.engine.blob.BlobIF.BlobSource;
+import com.github.adsgray.gdxtry1.engine.blob.BlobIF.BlobTransform;
+import com.github.adsgray.gdxtry1.engine.blob.BlobIF.BlobTrigger;
 import com.github.adsgray.gdxtry1.engine.position.BlobPosition;
 import com.github.adsgray.gdxtry1.engine.position.PositionIF;
 import com.github.adsgray.gdxtry1.engine.velocity.BlobVelocity;
@@ -30,5 +34,33 @@ public class TestFactory {
     public static RenderConfigIF renderConfig() {
         return renderer().nullRenderConfig;
     }
+    
+    // to use in testing to check how many times the methods are called
+    public static class TestBlobSource extends BlobSource {
+        public int num;
+        public TestBlobSource() { num = 0; }
 
+        @Override protected BlobIF generate(BlobIF parent) {
+            num += 1;
+            return parent;
+        }
+    }
+    
+    public static class TestBlobTransform extends BlobTransform {
+        public int num;
+        public TestBlobTransform() { num = 0; }
+        @Override public BlobIF transform(BlobIF b) {
+            num += 1;
+            return b;
+        }
+    }
+
+    public static class TestBlobTrigger extends BlobTrigger {
+        int num;
+        public TestBlobTrigger() { num = 0; }
+        @Override public BlobIF trigger(BlobIF source, BlobIF secondary) {
+            num += 1;
+            return source;
+        }
+    }
 }
