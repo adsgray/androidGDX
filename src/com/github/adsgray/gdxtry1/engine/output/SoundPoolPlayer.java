9340e7c6dcc82f91cbfe519eaad8a00d689da44e
Andrew Gray <adsgray@gmail.com>
2014-01-28 20:39:22 -0800

    make rate optional constructor param in SoundPoolPlayer

--- src/com/github/adsgray/gdxtry1/engine/output/SoundPoolPlayer.java
+++ src/com/github/adsgray/gdxtry1/engine/output/SoundPoolPlayer.java
@@ -9,19 +9,24 @@ public class SoundPoolPlayer implements SoundIF {
 
     private SoundPool sounds;
     Context context;
+    private float rate;
 
-    public SoundPoolPlayer(Context context) {
+    public SoundPoolPlayer(Context context, float rate) {
         this.context = context;
         sounds = new SoundPool(20, AudioManager.STREAM_MUSIC,0);
+        this.rate = rate;
     }
 
+    public SoundPoolPlayer(Context context) {
+      this(context, 1.5f);
+    }
     @Override public int load(int resid) {
         return sounds.load(context, resid, 1);
     }
     
     // sound must have been loaded and returned by load()
     @Override public void play(int soundid) {
-        sounds.play(soundid, 1.0f, 1.0f, 0, 0, 1.5f);
+        sounds.play(soundid, 1.0f, 1.0f, 0, 0, rate);
     }
 
     @Override public void play(Integer soundid) { play((int)soundid); }
